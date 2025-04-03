ae2b150cd601a7bcc2870ab7f3b9f25dd1393bdd
Ainz55 <azizandijoni@gmail.com>
2025-04-03 21:24:00 +0300

    started project

---
+++ main.py
@@ -0,0 +1,5 @@
+# Библиотеки (django, pillow)
+# создание проекта на django
+# django-admin - специальная команда для запуска других комманд
+# django-admin startproject project-name -  создание нового проекта
+# python manage.py runserver - запускает тестовый сервер
