ab21740dcb064ec69fa5a11ab2f6dde8246d3638
Ainz55 <azizandijoni@gmail.com>
2025-04-03 21:58:01 +0300

    added web_site app

--- main.py
+++ main.py
@@ -3,3 +3,4 @@
 # django-admin - специальная команда для запуска других комманд
 # django-admin startproject project-name -  создание нового проекта
 # python manage.py runserver - запускает тестовый сервер
+# python manage.py migrate - отправляет файл с миграциями на выполнение в базу
