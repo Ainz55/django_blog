744d51ff52fa88f349683cdc823c7ce68d2c6627
Ainz55 <azizandijoni@gmail.com>
2025-04-07 18:44:52 +0300

    added pagination and working with home page

--- blog/web_site/urls.py
+++ blog/web_site/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from . import views
 
-
 urlpatterns = [
-    path('', views.home_view, name='home')
+    path('', views.home_view, name='home'),
+    path('categories/<str:category_id>/', views.category_articles,
+         name='category_articles'),
 ]
--- blog/web_site/views.py
+++ blog/web_site/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render, HttpResponse
-from .models import Article
+from .models import Article, Category
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 
 def home_view(request):
     articles = Article.objects.all()
+    paginator = Paginator(articles, 3)
+    page = request.GET.get('page')
+    articles = paginator.get_page(page)
+
+    context = {
+        "articles": articles
+    }
+    return render(request, 'web_site/index.html', context)
+
+
+def category_articles(request, category_id):
+    category = Category.objects.get(pk=category_id)
+    articles = category.articles.all()
     context = {
         "articles": articles
     }
